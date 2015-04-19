7d1ec5297aae284e5a645ee598abef4a5081d88b
Jonathan Tumulak <tumulak.jonathan@gmail.com>
2015-04-19 18:49:51 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+import py2exe
+
+
+setup(
+  options = {
+    "py2exe":{
+      "dll_excludes": ["MSVCP90.dll"],
+    }
+  },
+  console = [{'script': 'assembercode.py'}]
+)
+
+# setup(console=['assembercode.py'])
